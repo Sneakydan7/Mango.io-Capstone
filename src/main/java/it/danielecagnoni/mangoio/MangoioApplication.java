ada262451acbdd366ef3cc0956635230c451feac
Daniele Cagnoni <145670818+Sneakydan7@users.noreply.github.com>
2024-03-05 17:55:13 +0100

    Initial commit

---
+++ src/main/java/it/danielecagnoni/mangoio/MangoioApplication.java
@@ -0,0 +1,13 @@
+package it.danielecagnoni.mangoio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangoioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangoioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/danielecagnoni/mangoio/MangoioApplicationTests.java
@@ -0,0 +1,13 @@
+package it.danielecagnoni.mangoio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangoioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
